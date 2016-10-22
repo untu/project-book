ae57231853bab390a8d87c27cdf30e2b3e066e54
Gregory Komagurov <grkov90@gmail.com>
2016-10-22 18:35:57 +0300

    Remove typings

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/angular2-compilers/index.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/meteor/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
