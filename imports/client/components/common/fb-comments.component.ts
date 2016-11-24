769a6835e0151aa2da4129cc9ef289fb44f2ea74
Gregory Komagurov <grkov90@gmail.com>
2016-11-24 11:37:39 +0300

    Fix bug with fbcomments zone.

--- imports/client/components/common/fb-comments.component.ts
+++ imports/client/components/common/fb-comments.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, NgZone} from "@angular/core";
 
 declare let FB;
 
@@ -6,19 +6,21 @@ declare let FB;
   selector: 'fb-comments',
   template: `
     <div *ngIf="!fbLoaded" style="position: absolute; margin: 10px 10px" class="loader"></div>
-    <div [hidden]="!fbLoaded" class="fb-comments" [attr.data-href]="href" data-width="100%" data-numposts="5"></div>
+    <div [hidden]="!fbLoaded" class="fb-comments" [attr.data-href]="href" data-width="100%" data-numposts="5"></div>    
     `
 })
 export class FbCommentsComponent implements OnInit {
   private fbLoaded: Boolean = false;
 
-  constructor(private el: ElementRef) {
+  constructor(private el: ElementRef,
+              private zone: NgZone) {
   }
 
   ngOnInit() {
-    FB.Event.subscribe('xfbml.render', () => {
-      this.fbLoaded = true
-    });
+    FB.Event.subscribe('xfbml.render', () =>
+      this.zone.run(() => {
+        this.fbLoaded = true
+      }));
 
     FB.XFBML.parse(this.el.nativeElement);
   }
